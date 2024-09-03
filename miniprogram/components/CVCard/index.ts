498eb51bcc3150ad2c990f6381bd94f550b90eab
Aaron Li <aaronlee916@qq.com>
2024-09-03 09:18:30 +0800

    added cvcard

---
+++ miniprogram/components/CVCard/index.ts
@@ -0,0 +1,27 @@
+// components/CVCard/index.ts
+Component({
+
+  /**
+   * 组件的属性列表
+   */
+  properties: {
+    userId:{
+      type:String,
+      value:''
+    }
+  },
+
+  /**
+   * 组件的初始数据
+   */
+  data: {
+    src:''
+  },
+
+  /**
+   * 组件的方法列表
+   */
+  methods: {
+    
+  }
+})
