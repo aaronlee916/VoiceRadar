8c8bd1b17b66fe32d154c09ae51d08e4de386118
Aaron Li <aaronlee916@qq.com>
2024-09-03 12:09:41 +0800

    created cvcard component

--- miniprogram/components/CVCard/index.ts
+++ miniprogram/components/CVCard/index.ts
@@ -1,13 +1,14 @@
 // components/CVCard/index.ts
+import { data } from '../../assets/data/cvdata'
 Component({
 
   /**
    * 组件的属性列表
    */
   properties: {
-    userId:{
-      type:String,
-      value:''
+    userId: {
+      type: Number,
+      value: 0
     }
   },
 
@@ -15,13 +16,41 @@ Component({
    * 组件的初始数据
    */
   data: {
-    src:''
+    avatarSrc: '',
+    name: '',
+    linkedUserId: -1,
+    isCV: false,
+    isStaff: false,
+    sex: '',
+    voiceType: '',
+    soundPressure: '',
+    voiceAge: -1,
+    demoLink: '',
+    description: ''
   },
-
+  ready() {
+    this.setAttr()
+  },
   /**
    * 组件的方法列表
    */
   methods: {
-    
+    setAttr() {
+      this.setData(
+        {
+          avatarSrc: data[this.properties.userId].avatarLink,
+          name: data[this.properties.userId].name,
+          linkedUserId: data[this.properties.userId].linkedUserId,
+          isCV: data[this.properties.userId].isCV,
+          isStaff: data[this.properties.userId].isStaff,
+          sex: data[this.properties.userId].sex,
+          voiceType: data[this.properties.userId].voiceType,
+          soundPressure: data[this.properties.userId].soundPressure,
+          voiceAge: data[this.properties.userId].voiceAge,
+          demoLink: data[this.properties.userId].demoLink,
+          description: data[this.properties.userId].description
+        }
+      )
+    }
   }
 })
